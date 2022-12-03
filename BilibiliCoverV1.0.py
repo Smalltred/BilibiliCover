8428f57ce25937fb14796e1fa6a46cf2edf73272
Small tred <1842244757@qq.com>
2022-12-03 15:22:15 +0800

    重构代码

---
+++ BilibiliCoverV1.0.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/3 15:22
+# @Author  : Small tred
+# @FileName: BilibiliCoverV1.0.py
+# @Software: PyCharm
+# @Blog    ：https://www.hecady.com
