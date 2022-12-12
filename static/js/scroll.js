5313b8746f4d05683e21d8db09c0b4ad802fb79e
Small tred <1842244757@qq.com>
2022-12-12 19:38:26 +0800

    feat:新增一键返回顶端

---
+++ static/js/scroll.js
@@ -0,0 +1,12 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: document.querySelector('#top').offsetTop,
+        behavior: 'smooth'
+    }, );
+}
+// function scrollToTop() {
+//     window.scrollTo({
+//         top: document.querySelector('#top').offsetTop,
+//         behavior: 'smooth'
+//     }, 2000);
+// }
