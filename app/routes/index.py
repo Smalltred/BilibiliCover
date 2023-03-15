47085eab4853d6faae730017489b689b510fd428
Small tred <1842244757@qq.com>
2023-03-16 00:08:56 +0800

    feat:对路由进行划分

--- app/routes/index.py
+++ app/routes/index.py
@@ -5,15 +5,12 @@
 # @FileName: routes.py
 # @Software: PyCharm
 # @Blog    : https://www.hecady.com
+import threading
 from flask import jsonify, render_template, request, abort, redirect, Blueprint
 from app.service.bilibiliCover import BilibiliCover
+from app.service.db_operate import DataPreprocessor
 
 index_bp = Blueprint("index", __name__, "")
-
-
-@index_bp.route("/")
-def index():
-    return render_template("index.html")
 
 
 @index_bp.route("/page")
@@ -47,42 +44,34 @@ def bilibiliApi():
 
 
 @index_bp.route("/", methods=["GET", "POST"])
-def handleResult():
-    if request.method == "POST":
-        data = request.form.get("text")
-        bilibili = BilibiliCover(data)
-        result = bilibili.get_cover()
-        if result is not None:
-            if result.get("code") == 200:
-                video_data = result.get("data")
-                if video_data is not None:
-                    # 多个视频
-                    if isinstance(video_data, list):
-                        return render_template("covers.html", result=video_data)
-                    # 单个视频
-                    elif isinstance(video_data, dict):
-                        # states = 1 上线 states = 0 没上线
-                        if video_data.get("states") == 1:
-                            # 番剧上线了 有ep 有pv
-                            if video_data.get("eps") is not None and video_data.get("pvs") is not None:
-                                eps = video_data.get("eps")
-                                pvs = video_data.get("pvs")
-                                return render_template("mds.html", pvs=pvs, eps=eps, result=video_data)
-                            # 番剧上线了 有ep 没有pv
-                            else:
-                                eps = video_data.get("eps")
-                                return render_template("ep.html", eps=eps, result=video_data)
-                        # 番剧没上线 只有pv
-                        elif video_data.get("states") == 0:
-                            pvs = video_data.get("pvs")
-                            return render_template("pv.html", pvs=pvs, result=video_data)
-                        else:
-                            # 视频单p
-                            return render_template("cover.html", result=video_data, )
-            elif result.get("code") == 403:
-                return render_template("error.html", result=result)
-            else:
-                return abort(404)
+def index():
+    if request.method != "POST":
+        return render_template("index.html")
+    data = request.form.get("text")
+    result = BilibiliCover(data).get_cover()
+    DataPreprocessor(data).process_data()
+    if result is None:
+        return render_template("error.html", result={"msg": "获取不到封面哦~"})
 
+    code, video_data = result.get("code"), result.get("data")
+    if code != 200:
+        return abort(404)
+
+    if isinstance(video_data, list):
+        return render_template("covers.html", result=video_data)
+
+    states = video_data.get("states")
+
+    if states == 1:
+        eps, pvs = video_data.get("eps"), video_data.get("pvs")
+        if eps is not None and pvs is not None:
+            return render_template("mds.html", pvs=pvs, eps=eps, result=video_data)
         else:
-            return render_template("error.html", result={"msg": "获取不到封面哦~"})
+            return render_template("mds.html", eps=eps, result=video_data)
+
+    elif states == 0:
+        pvs = video_data.get("pvs")
+        return render_template("pv.html", pvs=pvs, result=video_data)
+
+    else:
+        return render_template("cover.html", result=video_data)
