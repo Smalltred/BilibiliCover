68ef11024177476e5cfc7a18fa440c7a63b50aea
Small tred <1842244757@qq.com>
2023-03-16 00:09:53 +0800

    add:init

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,17 @@
 # @Blog    : https://www.hecady.com
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
-
+    # 初始化数据库
+    from app.models.db import VdCover, MdCovers
+    db.init_app(app)
     # 加载蓝图
     from app.routes.index import index_bp
     app.register_blueprint(index_bp)
