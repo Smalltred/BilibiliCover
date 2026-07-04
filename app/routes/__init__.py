6c76ac43bd69cab60e092bd692e747a5756361e1
Small_tred <admin@hecady.com>
2026-07-04 11:14:15 +0800

    chore: 清理根目录 Flask 残留文件
    老 Flask 代码(app/、config.py、manager.py、requirements.txt)
    已完整保存在 flask-legacy 分支,master 只保留新版 Node.js + Vue 3。

--- app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3 
-# -*- coding: utf-8 -*-
-# @Time    : 2023/3/14 19:22
-# @Author  : Small tred
-# @FileName: __init__.py
-# @Software: PyCharm
-# @Blog    : https://www.hecady.com
-from .api import api
--- app/service/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3 
-# -*- coding: utf-8 -*-
-# @Time    : 2023/3/15 11:15
-# @Author  : Small tred
-# @FileName: __init__.py.py
-# @Software: PyCharm
-# @Blog    : https://www.hecady.com
-from .bilibiliCover import BilibiliCover
